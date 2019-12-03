1fc1df0e127cee4c7ceb9000ab6c6bd06b17929b
silencespeaker <b06902019@ntu.edu.tw>
2019-12-03 19:25:10 +0800

    hope

---
+++ ALU_Control.v
@@ -0,0 +1,19 @@
+module ALU_Control
+(
+	func_i,
+	ALUOp_i,
+	ALUCtrl_o
+);
+
+// Ports
+
+input  [3:0]		func_i;
+input  [1:0]		ALIOp_i;
+output [2:0]		ALUCtrl_o;
+
+
+// Wires and Registers
+
+// Assignment
+
+endmodule
