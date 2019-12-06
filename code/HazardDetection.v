5ca94201e58248e25ccb755ef322c2064c95c996
silencespeaker <b06902019@ntu.edu.tw>
2019-12-07 01:41:44 +0800

    add HazardDetection but still not connect in CPU.v

---
+++ code/HazardDetection.v
@@ -0,0 +1,16 @@
+module HazardDetection(
+	input MemRead_EX_i,
+	input [4:0]	RS1addr_ID_i,
+	input [4:0]	RS2addr_ID_i,
+	input [4:0] RDaddr_EX_i,
+	output PCupdate_o,
+	output Hazard_o,
+	output stall_o
+);
+
+assign Hazard_o = (MemRead_EX_i && ((RS1addr_ID_i == RDaddr_EX_i) || (RS2addr_ID_i == RDaddr_EX_i))) ? 1 : 0;
+assign stall_o = Hazard_o;
+assign PCupdate_o = ~Hazard_o;
+
+
+endmodule
