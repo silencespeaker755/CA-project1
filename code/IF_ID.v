413201f1d59d6bae47693bc641a45d8827145424
Lu-YuCheng <b06902051@csie.ntu.edu.tw>
2019-12-06 15:56:53 +0800

    Change the time pipeline register get the input value from input wire by using rst_i

--- code/IF_ID.v
+++ code/IF_ID.v
@@ -1,6 +1,8 @@
 // Pipeline register for IF/ID
 module IF_ID(
     clk_i,
+    rst_i,
+    start_i,
     pc_i,
     instr_i,
     pc_o,
@@ -9,15 +11,29 @@ module IF_ID(
 
 // Ports
 input               clk_i;
+input               rst_i;
+input               start_i;
 input       [31:0]  pc_i;
 input       [31:0]  instr_i;
 output  reg [31:0]  pc_o;
 output  reg [31:0]  instr_o;
 
 // Assignment
-always@(posedge clk_i) begin
-    pc_o <= pc_i;
-    instr_o <= instr_i;
+always@(posedge clk_i or negedge rst_i) begin
+    if(~rst_i) begin
+        pc_o <= 32'b0;
+        instr_o <= 32'b0;
+    end
+    else begin
+        if(start_i) begin
+            pc_o <= pc_i;
+            instr_o <= instr_i;
+        end
+        else begin
+            pc_o <= 32'b0;
+            instr_o <= 32'b0;
+        end
+    end
 end
 
 endmodule
--- code/MEM_WB.v
+++ code/MEM_WB.v
@@ -1,6 +1,8 @@
 // MEM/WB pipeline register
 module MEM_WB(
     clk_i,
+    rst_i,
+    start_i,
     RegWrite_i,
     Memdata_i,
     ALUResult_i,
@@ -15,6 +17,8 @@ module MEM_WB(
 );
 // Ports
 input                   clk_i;
+input                   rst_i;
+input                   start_i;
 input                   RegWrite_i;
 input       [31:0]      Memdata_i;
 input       [31:0]      ALUResult_i; 
@@ -28,12 +32,30 @@ output  reg             MemtoReg_o;
 output  reg [4:0]       RDaddr_o;
 
 // Assignment
-always@(posedge clk_i) begin
-    RegWrite_o      <=   RegWrite_i;
-    Memdata_o       <=   Memdata_i;
-    ALUResult_o     <=   ALUResult_i;
-    MemtoReg_o      <=   MemtoReg_i;
-    RDaddr_o        <=   RDaddr_i;
+always@(posedge clk_i or negedge rst_i) begin
+    if(~rst_i) begin
+        RegWrite_o      <=   1'b0;
+        Memdata_o       <=   32'b0;
+        ALUResult_o     <=   32'b0;
+        MemtoReg_o      <=   1'b0;
+        RDaddr_o        <=   5'b0;
+    end
+    else begin
+        if(start_i) begin
+            RegWrite_o      <=   RegWrite_i;
+            Memdata_o       <=   Memdata_i;
+            ALUResult_o     <=   ALUResult_i;
+            MemtoReg_o      <=   MemtoReg_i;
+            RDaddr_o        <=   RDaddr_i;
+        end
+        else begin
+            RegWrite_o      <=   1'b0;
+            Memdata_o       <=   32'b0;
+            ALUResult_o     <=   32'b0;
+            MemtoReg_o      <=   1'b0;
+            RDaddr_o        <=   5'b0;
+        end
+    end
 end
 
 endmodule
